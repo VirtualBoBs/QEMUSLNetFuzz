3922236395769a6b316c63b2ace1f3a6316e11c2
candymate <candymate@github.com>
2020-10-08 21:09:35 +0900

    Added TCP/UDP, IPOption

--- converter.py
+++ converter.py
@@ -3,6 +3,12 @@
 from scapy.all import *
 
 def ip_vec_to_pkt(vec):
-  print(vec)
   return IP(ihl=vec[0], tos=vec[1], len=vec[2], id=vec[3], flags=vec[4], frag=vec[5], \
-    ttl=vec[6], proto=vec[7])/vec[8]
+    ttl=vec[6], proto=vec[7], options=IPOption(vec[8]))
+
+def tcp_vec_to_pkt(vec):
+  return TCP(sport=vec[0], dport=vec[1], seq=vec[2], ack=vec[3], dataofs=vec[4], \
+    reserved=vec[5], flags=vec[6], window=vec[7], urgptr=vec[8], options=TCPOption(vec[9]))
+
+def udp_vec_to_pkt(vec):
+  return UDP(sport=vec[0], dport=vec[1], len=vec[2])
--- randomizer.py
+++ randomizer.py
@@ -2,19 +2,39 @@
 
 import random
 
-# [20, 21, 23, 80, 513, 544, 543, 6667, 6668, 7070, 113, random.randint(1, 65535)]
-
 def new_ip_vec():
   # fields
-  ihl = None if random.random() > 0.93 else random.randint(20, 60) # protocol specification says max 15 -> 15 * 4 + 1
-  tos = 0x0 if random.random() > 0.93 else random.randint(0, 2**6-1)
-  _len = None if random.random() > 0.93 else random.randint(0, 2**16-1)
-  _id = 1 if random.random() > 0.93 else random.randint(0, 2**16-1)
-  flags = 0 if random.random() > 0.93 else random.randint(0, 2**3-1)
-  frag = 0 if random.random() > 0.93 else random.randint(0, 2**13-1)
-  ttl = 64 if random.random() > 0.93 else random.randint(0, 2**8-1)
-  proto = 0x0 if random.random() > 0.93 else random.randint(0, 2**8-1)
-  options = b"" if random.random() > 0.93 else bytes(bytearray(random.getrandbits(8) for _ in range(random.randint(0, 1500-24))))
-
+  ihl = None if random.random() < 0.9 else random.randint(20, 60) # protocol specification says max 15 -> 15 * 4 + 1
+  tos = 0x0 if random.random() < 0.9 else random.randint(0, 2**6-1)
+  _len = None if random.random() < 0.9 else random.randint(0, 2**16-1)
+  _id = 1 if random.random() < 0.9 else random.randint(0, 2**16-1)
+  flags = 0 if random.random() < 0.9 else random.randint(0, 2**3-1)
+  frag = 0 if random.random() < 0.9 else random.randint(0, 2**13-1)
+  ttl = 64 if random.random() < 0.9 else random.randint(0, 2**8-1)
+  proto = 0x0 if random.random() < 0.9 else random.randint(0, 2**8-1)
+  options = b"" if random.random() < 0.9 else bytes(bytearray(random.getrandbits(8) for _ in range(random.randint(0, 1500-24))))
   return (ihl, tos, _len, _id, flags, frag, ttl, proto, options)
 
+def new_tcp_vec():
+  # fields
+  sport = random.randint(0, 2**16-1)
+  dport = random.choice([20, 21, 23, 80, 513, 544, 543, 6667, 6668, 7070, 113]) if random.random() < 0.9 else random.randint(0, 2**16-1)
+  seq = 0 if random.random() < 0.9 else random.randint(0, 2**32-1)
+  ack = 0 if random.random() < 0.9 else random.randint(0, 2**32-1)
+  dataofs = None if random.random() < 0.9 else random.randint(0, 2**4-1)
+  reserved = 0 if random.random() < 0.9 else random.randint(0, 2**3-1)
+  flags = 2 if random.random() < 0.9 else random.randint(0, 2**9-1)
+  window = 8192 if random.random() < 0.9 else random.randint(0, 2**16-1)
+  urgptr = 0 if random.random() < 0.9 else random.randint(0, 2**16-1)
+  options = b"" if random.random() < 0.9 else bytes(bytearray(random.getrandbits(8) for _ in range(random.randint(0, 1500-24-20))))
+  return (sport, dport, seq, ack, dataofs, reserved, flags, window, urgptr, options)
+
+def new_udp_vec():
+  # fields
+  sport = random.randint(0, 2**16-1)
+  dport = random.choice([20, 21, 23, 80, 513, 544, 543, 6667, 6668, 7070, 113]) if random.random() < 0.9 else random.randint(0, 2**16-1)
+  _len = None if random.random() < 0.9 else random.randint(0, 2**16-1)
+  return (sport, dport, _len)
+
+def new_raw(hlen): # gets header length
+  return b"" if random.random() < 0.9 else bytes(bytearray(random.getrandbits(8) for _ in range(random.randint(0, 1500-hlen))))
