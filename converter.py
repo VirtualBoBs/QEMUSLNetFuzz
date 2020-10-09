7ba42d550047e0e9ba4fff585cef7482e2d16973
candymate <candymate@github.com>
2020-10-09 21:59:42 +0900

    Revert "IP packet random generator complete"
    This reverts commit 4719ec78b703907b31d94161342f8567d5cf76dc.

--- converter.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-from scapy.all import *
-
-def ip_vec_to_pkt(vec):
-  print(vec)
-  return IP(ihl=vec[0], tos=vec[1], len=vec[2], id=vec[3], flags=vec[4], frag=vec[5], \
-    ttl=vec[6], proto=vec[7])/vec[8]
--- target.py
+++ target.py
@@ -3,15 +3,12 @@
 import socket
 import scapy
 
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
 
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
 
-if __name__ == "__main__":
-  HOST = '127.0.0.1'  # The server's hostname or IP address
-  PORT = 65432        # The port used by the server
-
-  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-      s.connect((HOST, PORT))
-      s.sendall(b'Hello, world')
-      data = s.recv(1024)
-
-  print('Received', repr(data))
+print('Received', repr(data))
